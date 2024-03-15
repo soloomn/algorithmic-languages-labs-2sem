9a5628dc8adbaa21ad0851282dbb68fc856cae1b
Tim <resn2p@gmail.com>
2024-03-16 00:06:06 +0300

    add _getch() comment

--- lab3/lab3_11.cpp
+++ lab3/lab3_11.cpp
@@ -123,6 +123,7 @@ int main()
 	cout << !a;
 	cout << "The length of the circle a: ";
 	cout << ~a;
-	_getch();
+	_getch(); //the standard windows terminal sometimes closes automatically after code execution, with the empty command "_getch()"
+	//let's put it in "standby mode" before entering any character via <Enter> (if you use the standard MS VS Code terminal, this command can be removed)
 	return 0;
 }
